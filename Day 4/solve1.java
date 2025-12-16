6459d3e0e041d5c959298e37a08a0d53f4864b67
Haloen <72333894+Haloen@users.noreply.github.com>
2025-12-16 22:20:54 +0800

    Day 4 is for JAVA

---
+++ Day 4/solve1.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) throws FileNotFoundException {
+        File input = new File("input.txt");
+        try (Scanner scanner = new Scanner(input)) {
+            scanner.useDelimiter("\\A");
+            String grid[] = scanner.next().split("\n");
+
+            int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+            int[] dy = {1, 1, 0, -1, -1, -1, 0, 1};
+            int ans = 0;
+
+            for (int i = 0; i < grid.length; ++i) {
+                for (int j = 0; j < grid[0].length(); ++j) {
+                    if (grid[i].charAt(j) != '@') continue;
+                    int count = 0;
+                    for (int k = 0; k < 8; ++k) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (nx < 0 || grid.length <= nx) continue;
+                        if (ny < 0 || grid[0].length() <= ny) continue;
+
+                        if (grid[nx].charAt(ny) != '@') continue;
+                        ++count;
+                    }
+                    if (count >= 4) continue;
+                    ++ans;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
